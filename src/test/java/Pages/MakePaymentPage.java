433bb83fa0dc3a72910b23117bed2b7338a5e661
vikenty19 <vikenty.plakhov@testpro.io>
2024-02-09 18:19:05 -0500

    paymentDataTest brought to POM

--- src/test/java/Pages/MakePaymentPage.java
+++ src/test/java/Pages/MakePaymentPage.java
@@ -25,7 +25,13 @@ public class MakePaymentPage extends BasePage {
     By iframe3id = By.id("fullsteam-hosted-expiration-year-frame");
     By choseYear = By.cssSelector("select#expiration-year option:nth-child(6)");
     By year = By.cssSelector("select#expiration-year option:nth-child(3)");
-
+    By iframe4id = By.cssSelector("#fullsteam-hosted-cvv-frame");
+    By cvvLocator = By.cssSelector("input#cvv");
+    By sbmtLocator = By.cssSelector("[data-name='Submit Button']");
+    By alertLocator = By.xpath("//div/p[contains(text(),'Enter Address')]");
+    By fridgeCss = By.cssSelector(" #extra-66 div img ");
+    By insideFridgeLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-title");
+    By fridgePriceLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-total");
     public void clickBookPage() {
         WebElement bookNowBtn = wait.until(ExpectedConditions.elementToBeClickable(bookXml));
         bookNowBtn.click();
@@ -37,6 +43,35 @@ public class MakePaymentPage extends BasePage {
         driver.switchTo().frame(iframe1);
     }
 
+    public void pickTheMonth() {
+        WebElement iframe2 = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(iframe2id));
+        driver.switchTo().frame(iframe2);
+        WebElement pickMonthField = wait.until(ExpectedConditions
+                .elementToBeClickable(choseMonth));
+        pickMonthField.click();
+        WebElement monthPicker = wait.until(ExpectedConditions
+                .elementToBeClickable(month));
+        monthPicker.click();
+    }
+    public void pickTheYear() {
+        WebElement iframe3 = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(iframe3id));
+        driver.switchTo().frame(iframe3);
+        WebElement pickYearField = wait.until(ExpectedConditions
+                .elementToBeClickable(choseYear));
+        pickYearField.click();
+        WebElement yearPicker = wait.until(ExpectedConditions
+                .elementToBeClickable(year));
+        yearPicker.click();
+    }
+    public void enterCvvCode() {
+        driver.switchTo().frame(wait.until((ExpectedConditions
+                .visibilityOfElementLocated(iframe4id))));
+        WebElement cvv= wait.until(ExpectedConditions.elementToBeClickable(cvvLocator));
+        cvv.click();
+        cvv.sendKeys("345");
+    }
     public void switchToMainIframe() throws InterruptedException {
         WebElement iframe = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(iframeXpath));
@@ -63,5 +98,15 @@ public class MakePaymentPage extends BasePage {
                 .elementToBeClickable(zip));
         js.executeScript("arguments[0].click();", zipCode);
         zipCode.sendKeys("12345");
+    }
+    public void clickSubmitBtn() {
+        WebElement submitBtn = wait.until(ExpectedConditions.elementToBeClickable(sbmtLocator));
+        submitBtn.click();
+    }
+    public void isAlertDisplayed() {
+        WebElement alertBanner = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(alertLocator));
+        Assert.assertTrue(alertBanner.isDisplayed());
+
     }
 }
--- src/test/java/Tests/MakePaymentTests.java
+++ src/test/java/Tests/MakePaymentTests.java
@@ -11,90 +11,39 @@ import org.testng.annotations.Test;
 public class MakePaymentTests extends BaseTest {
 
 
- By name = By.cssSelector("#NameOnAccount");
- By zip = By.name("BillingZipCode");
- By iframeXpath = By.xpath("//iframe[@data-lazyloaded = '1']");
- //By iframe1id = By.id("fullsteam-hosted-card-number-frame");
- By cardCss = By.cssSelector("[name='CardNumber']");
- By iframe2id = By.id("fullsteam-hosted-expiration-month-frame");
- By choseMonth = By.cssSelector("select#expiration-month");
- By month = By.cssSelector("select#expiration-month option:nth-child(5)");
- By iframe3id = By.id("fullsteam-hosted-expiration-year-frame");
- By choseYear = By.cssSelector("select#expiration-year option:nth-child(6)");
- By year = By.cssSelector("select#expiration-year option:nth-child(3)");
- By iframe4id = By.cssSelector("#fullsteam-hosted-cvv-frame");
- By cvvLocator = By.cssSelector("input#cvv");
- By sbmtLocator = By.cssSelector("[data-name='Submit Button']");
- By alertLocator = By.xpath("//div/p[contains(text(),'Enter Address')]");
-  By fridgeCss = By.cssSelector(" #extra-66 div img ");
-  By insideFridgeLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-title");
-  By fridgePriceLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-total");
+   
+    By fridgeCss = By.cssSelector(" #extra-66 div img ");
+    By insideFridgeLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-title");
+    By fridgePriceLocator = By.cssSelector(".shadow-border.summary-panel .extra-summary-total");
 
-    @Test(invocationCount = 4,successPercentage = 75)
+    @Test(invocationCount = 1, successPercentage = 75)
     public void paymentDataTest() throws InterruptedException {
         MakePaymentPage makePaymentPage = new MakePaymentPage(driver);
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         makePaymentPage.clickBookPage();
-          Thread.sleep(3000);
-         makePaymentPage.switchToMainIframe();
-
-
-        //Enter tne name of account
+        Thread.sleep(3000);
+        makePaymentPage.switchToMainIframe();
         makePaymentPage.enterTheUserName(js);
-
-        //     Thread.sleep(1000);
-        //Enter Zipcode
         makePaymentPage.enterZipCode(js);
-
-        //Enter card number
-       makePaymentPage.switchToCardEnterIframe();
-       makePaymentPage.enterCardNumber();
-       
-        driver.switchTo().parentFrame();
-      
-     //Enter the month
-     WebElement iframe2 = wait.until(ExpectedConditions
-             .visibilityOfElementLocated(iframe2id));
-     driver.switchTo().frame(iframe2);
-     WebElement pickMonthField = wait.until(ExpectedConditions
-             .elementToBeClickable(choseMonth));
-     pickMonthField.click();
-     WebElement monthPicker = wait.until(ExpectedConditions
-             .elementToBeClickable(month));
-     monthPicker.click();
+        makePaymentPage.switchToCardEnterIframe();
+        makePaymentPage.enterCardNumber();
 
         driver.switchTo().parentFrame();
 
-
-        // Enter the year
-        WebElement iframe3 = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(iframe3id));
-        driver.switchTo().frame(iframe3);
-        WebElement pickYearField = wait.until(ExpectedConditions
-                .elementToBeClickable(choseYear));
-        pickYearField.click();
-        WebElement yearPicker = wait.until(ExpectedConditions
-                .elementToBeClickable(year));
-        yearPicker.click();
+        makePaymentPage.pickTheMonth();
+        driver.switchTo().parentFrame()
+        ;
+        makePaymentPage.pickTheYear();
         driver.switchTo().parentFrame();
 
-        //Enter cvv
-        driver.switchTo().frame(wait.until((ExpectedConditions
-                .visibilityOfElementLocated(iframe4id))));
-       WebElement cvv= wait.until(ExpectedConditions.elementToBeClickable(cvvLocator));
-        cvv.click();
-        cvv.sendKeys("345");
-        //Submit
+        makePaymentPage.enterCvvCode();
         driver.switchTo().parentFrame();
-        WebElement submitBtn = wait.until(ExpectedConditions.elementToBeClickable(sbmtLocator));
-        submitBtn.click();
+
+        makePaymentPage.clickSubmitBtn();
         driver.switchTo().defaultContent();
         makePaymentPage.switchToMainIframe();
         //Assertion
-        WebElement alertBanner = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(alertLocator));
-        Assert.assertTrue(alertBanner.isDisplayed());
+        makePaymentPage.isAlertDisplayed();
         System.out.println("You made a reservation successfully ");
 
 
@@ -104,23 +53,21 @@ public class MakePaymentTests extends BaseTest {
 //alert locator div.alert-danger
     }
 
-@Test(invocationCount = 3)
-    public void addExtraService() throws InterruptedException {
-    MakePaymentPage makePaymentPage = new MakePaymentPage(driver);
-  //  JavascriptExecutor js = (JavascriptExecutor)driver;
 
-    makePaymentPage.clickBookPage();
-  //  Thread.sleep(3000);
-    makePaymentPage.switchToMainIframe();
-    wait.until(ExpectedConditions.elementToBeClickable(fridgeCss)).click();
-    WebElement addFridge = wait.until(ExpectedConditions
-            .visibilityOfElementLocated(insideFridgeLocator));
-    String insideFridge = addFridge.getText();
-   Assert.assertTrue(wait.until(ExpectedConditions
-           .textToBePresentInElementLocated(fridgePriceLocator,"25")));
-    Assert.assertEquals(insideFridge,"Inside the Fridge");
-    System.out.println(insideFridge);
-}
+    @Test(invocationCount = 3)
+    public void addExtraService() throws InterruptedException {
+        MakePaymentPage makePaymentPage = new MakePaymentPage(driver);
+        makePaymentPage.clickBookPage();
+        makePaymentPage.switchToMainIframe();
+        wait.until(ExpectedConditions.elementToBeClickable(fridgeCss)).click();
+        WebElement addFridge = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(insideFridgeLocator));
+        String insideFridge = addFridge.getText();
+        Assert.assertTrue(wait.until(ExpectedConditions
+                .textToBePresentInElementLocated(fridgePriceLocator, "25")));
+        Assert.assertEquals(insideFridge, "Inside the Fridge");
+        System.out.println(insideFridge);
+    }
 
 
 }
